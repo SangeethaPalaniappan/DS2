6db0820f9dee1822ac2bdc624ab297412c1e536a
Sangeetha <sangeethapa2002@gmail.com>
2023-07-26 15:45:04 +0530

    Corrections rectified

--- DS2/Graph/Implementation using Linkedlist.py
+++ DS2/Graph/Implementation using Linkedlist.py
@@ -1,20 +1,22 @@
-#Need to write function for the distance searching
+#Need to write function for the distance searching -completed
+#Need to search which is the shortest distance
 
 
 class Node:
     def __init__(self,val,key):
         
-        self.edge=val
-        self.data=key
+        self.edge=key
+        self.gver=val
         self.next=None
     
 class Linkedlist:
     def __init__(self):
         self.head=None
         
-    def insert(self,src,dest,key):
+    def insert(self,src,dest,key):#inserting the edge
         d=Node(dest,key)
-        if a[src]==None:
+        
+        if a[src]==None:#if there is no edge from the vertex
             self.head=d
             a[src]=d
         else:
@@ -22,35 +24,57 @@ class Linkedlist:
             while temp.next!=None:
                 temp=temp.next
             temp.next=d    
-    def delete(self,src,dest,key):
-        if a[src]==None:
-            print("There is no edge from",src,"to",dest)
-        else:
             
+    def delete(self,src,dest,key):#deleting the edge
+    
+        if a[src]==None:#if there is no edge from the vertex
+            print("There is no edge of weight",key," from",src,"to",dest)
+        
+        else:
             temp=self.head
-            if temp.data==key:
+            #to check whether the dest vertex is same or not
+                #else it will delete the edge of the given weight of any other vertex
+            
+            #Iterating to get the vertex to be deleted
+            while temp.gver!=dest:
+                temp=temp.next
+                if temp==None:
+                    print("There is no edge of weight",key," from",src,"to",dest)
+                    return
+            if  temp.edge==key:
                 self.head=temp.next
-                
+            
                 a[src]=temp.next
                 temp.next=None
+                return
             else:
                 while temp.next!=None:
-                    if temp.next.data==key:
+                    #even we found the vertex
+                    #It is necessary to check the vertex because
+                    #here I'm checking the temp's next so need to check the vertex and key
+                    
+                    if temp.next.gver==dest and temp.next.edge==key:
                         temp.next=temp.next.next
                         break
                     temp=temp.next    
                 else:
-                    print("There is no edge from",src,"to",dest)
+                    print("There is no edge of weight",key," from",src,"to",dest)
+                    return
+            
+            print("There is no edge of weight",key,"from",src,"to",dest)
 
-    def search(self,src,dest,key):
+    def search(self,src,dest,key):#searching the edge
         if a[src]==None:
             print("There is no edge from",src,"to",dest)
         else:
             temp=self.head
             while temp!=None:
-                if temp.data==key:
-                    print("Edege Found")
-                    break
+                #to check whether the dest vertex is same or not
+                #else it will delete the edge of the given weight of any other vertex
+                if temp.gver==dest:
+                    if temp.edge==key:
+                        print("Edege Found")
+                        break
                 temp=temp.next 
             else:
                 print("No Edge Found")
@@ -62,7 +86,7 @@ class Linkedlist:
         else:
             print(src,"-->",end="")
             while temp!=None:
-                print("\t",temp.edge,"[",temp.data,"]",end=" ")
+                print("\t",temp.gver,"[",temp.edge,"]",end=" ")
                 temp=temp.next
             
                     
@@ -71,9 +95,26 @@ a=[None]*5
 #a[1]=Node(0)
 #a[2]=Node(0)
 #a[3]=Node(2)
-#a[4]=Node(1)  
+#a[4]=Node(1) 
+#why seperated functions were written?
 s=Linkedlist()
 
+#else I can create a function for calling insert,delete and search 
+'''def func_insert():
+    n=int(input("No.of insertions:"))
+    for x in range(n):
+        ver1=int(input("ver1:"))
+        ver2=int(input("ver2:"))
+        edge=int(input("edge:"))
+        insert(ver1,ver2,edge)
+        
+def funct_delete():
+    n=int(input("No.of deletions:"))
+    for x in range(n):
+        ver1=int(input("ver1:"))
+        ver2=int(input("ver2:"))
+        edge=int(input("edge:"))
+        delete(ver1,ver2,edge)'''
 s.insert(0,1,5)
 s.insert(0,2,7)
 s.insert(0,3,9)
@@ -84,45 +125,43 @@ s.search(0,4,11)
 s.search(0,2,11)
 s.printg(0)
 
-print("\n")
-L=Linkedlist()
-L.insert(1,0,4)
-L.insert(1,2,2)
-L.insert(1,3,6)
-L.insert(1,4,12)
-L.delete(1,0,2)
-L.delete(1,0,4)
-L.delete(1,4,4)
-L.search(1,3,6)
-L.printg(1)
+s.insert(1,0,4)
+s.insert(1,2,2)
+s.insert(1,3,6)
+s.insert(1,4,12)
+s.delete(1,0,2)
+s.delete(1,0,4)
+s.delete(1,4,4)
+s.search(1,3,6)
+s.printg(1)
 
-print("\n")
-M=Linkedlist()
-M.insert(2,0,4)
-M.insert(2,1,5)
-M.insert(2,3,7)
-M.insert(2,4,8)
-M.delete(2,0,3)
-M.delete(2,0,4)
-M.insert(2,1,4)
-M.search(2,9,5)
-M.search(2,3,5)
-M.printg(2)
 
-print("\n")
+s.insert(2,0,4)
+s.insert(2,1,5)
+s.insert(2,3,7)
+s.insert(2,4,8)
+s.delete(2,0,3)
+s.delete(2,0,4)
+s.insert(2,1,4)
+s.search(2,9,5)
+s.search(2,3,5)
+s.printg(2)
 
-D=Linkedlist()
-D.insert(3,0,4)
-D.insert(3,1,3)
-D.insert(3,2,3)
-D.insert(3,4,3)
-D.delete(3,0,4)
-D.delete(3,0,6)
-D.printg(3)
+s.insert(3,0,4)
+s.insert(3,1,3)
+s.insert(3,2,3)
+s.insert(3,4,3)
+s.delete(3,0,4)
+s.delete(3,0,6)
+s.printg(3)
 
-print("\n")
-L.delete(4,0,3)
-L.search(4,5,2)
-L.printg(4)
 
-print("\n")
+s.delete(4,0,3)
+s.search(4,5,2)
+s.printg(4)
+
+#adding the vertex
+a.append(None)
+
+#deleting the vertex
+del(a[2])#2 is the position
