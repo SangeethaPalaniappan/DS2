428dc53359ec42f1767f9945a950dfa5ca24d57e
Sangeetha <sangeethapa2002@gmail.com>
2023-10-08 12:02:53 +0530

    trail

---
+++ OOPS/Exercises/4. Class.py
@@ -0,0 +1,3 @@
+# 4. Write a Python program to create a class that represents a shape. Include methods to calculate its area and perimeter.
+# Implement subclasses for different shapes like circle, triangle, and square.
+print("yes")
